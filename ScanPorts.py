d449a59477aadd067cd1e702669fc55d0ae1de9b
Oswaoldo Hernández Ávila <108857178+420Avila@users.noreply.github.com>
2022-09-05 19:32:03 -0500

    Update ScanPorts.py

--- ScanPorts.py
+++ ScanPorts.py
@@ -1,8 +1,22 @@
+#!/usr/bin/python3
 from ast import arguments
 from itertools import count
 from unittest import result
 import nmap
-
+print()
+print(" _  _ ___   ___              _ _       ")
+print("| || |__ \ / _ \            (_) |      ")
+print("| || |_ ) | | | | __ ___   ___| | __ _ ")
+print("|__   _/ /| | | |/ _` \ \ / / | |/ _` |")
+print("   | |/ /_| |_| | (_| |\ V /| | | (_| |")
+print("   |_|____|\___/ \__,_| \_/ |_|_|\__,_|")
+print()
+print() 
+print("+---------------------------------------------------------------------------+")
+print("| [Info] Herramienta para escanear los puertos abiertos en una dirección IP |")
+print("| * Escrito en Python y utiliza Nmap                                     |")
+print("+---------------------------------------------------------------------------+ ")
+print() 
 host= input ("[+] Introduce la Ip Objetivo: ")
 nm= nmap.PortScanner()
 puertos_abiertos= "-p"
